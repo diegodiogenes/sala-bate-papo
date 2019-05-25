0285156a1ec8154c2cbe821d77119ae35e6bd23d
Douglas <carvalhodouglas1999@gmail.com>
2019-05-25 15:41:40 -0300

    lista-add-servidor

--- Servidor.py
+++ Servidor.py
@@ -16,12 +16,15 @@ serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
 serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
 serverSocket.listen(1) # socket pronto para 'ouvir' conexoes
 print ('Servidor TCP esperando conexoes na porta %d ...' % (serverPort))
+lista_addr = []
 while 1:
   connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
+  lista_addr.append(addr) # Guarda os clientes conectados
   sentence = connectionSocket.recv(1024) # recebe dados do cliente
   sentence = sentence.decode('utf-8')
   capitalizedSentence = sentence.upper() # converte em letras maiusculas
   print ('Cliente %s enviou: %s, transformando em: %s' % (addr, sentence, capitalizedSentence))
   connectionSocket.send(capitalizedSentence.encode('utf-8')) # envia para o cliente o texto transformado
   connectionSocket.close() # encerra o socket com o cliente
+  print("Lista de clientes conectados: ", str(lista_addr))
 serverSocket.close() # encerra o socket do servidor
